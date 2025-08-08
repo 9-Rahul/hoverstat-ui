97cc30dd9ebe4bdb7f397a59cd623d4482aa9185
Rahul Thapliyal <9.rahul02@gmail.com>
2025-08-08 19:36:00 +0530

    Banner Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './My Components/Pages/Header';
+import Banner from './My Components/Pages/Banner';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
     </div>
   );
 }
