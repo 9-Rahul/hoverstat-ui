64dcc802d98603f6fa25cf9c1e041687cddeb841
Rahul Thapliyal <9.rahul02@gmail.com>
2025-08-08 16:28:58 +0530

    Header added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './My Components/Pages/Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
